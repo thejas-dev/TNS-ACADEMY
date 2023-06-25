e2bf6cdb99de828cc0fe65bd9a8e98595eb29715
thejas <thejaskala308@gmail.com>
2023-06-25 20:30:28 +0530

    make it better

--- pages/slideIn.js
+++ pages/slideIn.js
@@ -1,19 +1,13 @@
 import {Head} from 'next/head';
 import LoginComponent from '../components/LoginComponent'
 import {getProviders,getSession,useSession} from 'next-auth/react'
-import axios from 'axios';
 
 
-export const metadata = {
-  title: 'TNS-Academy',
-  description: 'Learn web development and Programming stuffs in <TNS academy/>. We faciliate a wide range of courses and workshops with a lot of useful, treding, and must know topics. Join for free and Start learning today.',
-}
 
 export default function slideIn({providers,session2}) {
-	// body...
-	// console.log(session2,providers)
+	
 	const {data:session} = useSession();
-	// console.log(session,session2);
+
 
 	return (
 		<>
--- pages/workshops.js
+++ pages/workshops.js
@@ -3,12 +3,8 @@ import WorkshopComponent from '../components/WorkshopComponent';
 import {useRouter} from 'next/navigation';
 import {useRecoilState} from 'recoil'
 import {currentWorkshopState,currentUserState} from '../atoms/userAtom';
-import {useState,useEffect} from 'react';
+import {useEffect} from 'react';
 
-export const metadata = {
-  title: 'TNS-Academy',
-  description: 'Learn web development and Programming stuffs in <TNS academy/>. We faciliate a wide range of courses and workshops with a lot of useful, treding, and must know topics. Join for free and Start learning today.',
-}
 
 export default function workshops() {
 	// body...
