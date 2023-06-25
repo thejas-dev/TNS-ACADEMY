36201aa2b200a19cddb2984b8d4eb4a0be0e9386
thejas <thejaskala308@gmail.com>
2023-06-25 15:21:31 +0530

    make it better

--- utils/ApiRoutes.js
+++ utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:3333";
+export const host = "https://tns-academy-server.vercel.app";
 
 export const getAllWorkshops = `${host}/api/auth/getAllWorkshops`;
 export const registerRoute = `${host}/api/auth/register`;
