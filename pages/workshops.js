2593c7bb94d1d7a0de2d73ba4693832d6dd237e4
thejas <thejaskala308@gmail.com>
2023-06-29 11:58:58 +0530

    make i tbetter

--- pages/workshops.js
+++ pages/workshops.js
@@ -5,7 +5,7 @@ import {useRouter} from 'next/navigation';
 import {useRecoilState} from 'recoil'
 import {currentWorkshopState,currentUserState} from '../atoms/userAtom';
 import {useEffect} from 'react';
-
+import Head from 'next/head';
 
 export default function Workshops() {
 	// body...
@@ -22,10 +22,14 @@ export default function Workshops() {
 	},[currentUser])
 
 	return (
-
+		<div>
+		<Head>
+	  		<title>TNS Academy</title>
+	  	</Head>
 		<main className="w-full h-screen overflow-y-scroll scrollbar-thin scroll-smooth bg-gray-100">
 			<WorkshopComponent />
 
 		</main>
+		</div>
 	)
 }
