75715b89c4e5a0157952581688cbb743e9866e83
thejas <thejaskala308@gmail.com>
2023-06-29 12:10:05 +0530

    make i tbetter

--- pages/workshops.js
+++ pages/workshops.js
@@ -6,6 +6,7 @@ import {useRecoilState} from 'recoil'
 import {currentWorkshopState,currentUserState} from '../atoms/userAtom';
 import {useEffect} from 'react';
 
+
 export default function Workshops() {
 	// body...
 	const router = useRouter();
@@ -21,14 +22,10 @@ export default function Workshops() {
 	},[currentUser])
 
 	return (
-		<div>
-		<Head>
-	  		<title>Workshops</title>
-	  	</Head>
+
 		<main className="w-full h-screen overflow-y-scroll scrollbar-thin scroll-smooth bg-gray-100">
 			<WorkshopComponent />
 
 		</main>
-		</div>
 	)
 }
