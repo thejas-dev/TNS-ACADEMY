51ff56b5cf1bd8f3c2706e85cfca30797dbe8f5d
thejas-dev <thejasgamerx123@gmail.com>
2024-01-01 14:14:11 +0530

    make

--- pages/index.js
+++ pages/index.js
@@ -65,7 +65,7 @@ export default function Home({session2}) {
   		<title>TNS Academy</title>
   		<link rel="icon" href="/favicon.ico" sizes="any" />
   		<meta property="og:url" content="https://tnsacademy.vercel.app" key="ogurl" />
-			<meta property="og:image" content="https://ik.imagekit.io/d3kzbpbila/thejashari_QSsM9TGJP" key="ogimage" />
+			<meta property="og:image" content="https://ik.imagekit.io/d3kzbpbila/thejashari_2s7bX4H_h" key="ogimage" />
 			<meta property="og:site_name" content="TNS-ACADEMY" key="ogsitename" />
 			<meta property="og:title" content="TNS-ACADEMY" key="ogtitle" />
 			<meta property="og:description" content="TNS academy is your online destination for learning various technologies. You will enjoy our fun and interactive video lessons, workshops and hands-on projects. No matter if you are a newbie or a pro, we will help you sharpen your skills to build amazing websites, apps, products, and more. Become part of our vibrant community of learners and express your creativity in the digital world. 
